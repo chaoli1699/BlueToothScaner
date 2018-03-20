d287d73eb93a43a348d566538fc81b305711e6f6
Aaron <chao_li1699@gmail.com>
2018-03-20 17:54:57 +0800

    add Scanview

--- src/com/example/bluetoothscaner/MainActivity.java
+++ src/com/example/bluetoothscaner/MainActivity.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.bluetoothscaner.bean.BleDevice;
+import com.example.bluetoothscaner.weight.ScanView;
+import com.example.bluetoothscaner.weight.ScanView.OnPointUpdateListener;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -12,7 +14,9 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,19 +25,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.ViewStub;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private Button scanBtn;
 	private TextView callbackText;
+	private RelativeLayout container;
+	private ScanView scanView;
 	private ListView bleList;
-	
-//	private BluetoothManager bluetoothManager;
+
 	private BluetoothAdapter bluetoothAdapter;
 	private List<BleDevice> deviceList;
 	private ScanThread scanThread;
@@ -42,75 +50,141 @@ public class MainActivity extends Activity {
 	private static final int REQUEST_BLE_ENABLE=2;
 	private ArrayAdapter<BleDevice> adapter;
 	private boolean blePrepared=false;
-	
+	private int count=0;
+	private List<Integer> pointX;
+	private List<Integer> pointY;
+
 	@SuppressLint("HandlerLeak")
 	private Handler handler=new Handler(){
-		
+
 		public void handleMessage(android.os.Message msg) {
 			switch (msg.what) {
 			case A_NEW_DEVICE_SCANED:
-				adapter.notifyDataSetChanged();
+//			    adapter.notifyDataSetChanged();
+				count++;
+				setMTextColor("已扫描到蓝牙设备("+count+")个...", "#ff00ff00");
+				pointX.add((int)(300*(Math.random()-1)));
+				pointY.add((int)(200*(Math.random()-1)));
 				break;
 
 			case SCAN_DEVICE_COMPLATE:
-				setMTextColor("已获得所有蓝牙设备！","#6699FF");
+				setMTextColor("已获得所有蓝牙设备("+count+"个)","#6699FF");
 				scanBtn.setText("重新扫描");
 				scanThread.interrupt();
+				count=0;
+
+				if (scanView!=null) {
+					scanView.stop();
+					scanView.setVisibility(View.GONE);
+				}
+
+				if (bleList==null) {
+					ViewGroup.LayoutParams bleListPL=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 
+							ViewGroup.LayoutParams.MATCH_PARENT);
+					bleList=new ListView(MainActivity.this);
+					bleList.setLayoutParams(bleListPL);
+					bleList.setVerticalScrollBarEnabled(false);
+					container.addView(bleList);	
+					adapter=new ArrayAdapter<BleDevice>(MainActivity.this, android.R.layout.simple_list_item_1, deviceList);
+					bleList.setAdapter(adapter);
+				}else {
+					if (bleList.getVisibility()==View.GONE) {
+						bleList.setVisibility(View.VISIBLE);
+						adapter.notifyDataSetChanged();
+					}
+				}
+				
 				break;
 			default:
 				break;
 			}
 		};
 	};
-	
-	
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		scanBtn=findViewById(R.id.main_scanBtn);
 		scanBtn.setText("开始扫描");
-				
+
 		scanBtn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				scanBle();
 			}
 		});
+
+		container=findViewById(R.id.main_container);
 	}
-	
+
 	private void scanBle() {
 		// TODO Auto-generated method stub
-		bleList=findViewById(R.id.main_BleList);
-		
-		if (deviceList==null) {
-			deviceList=new ArrayList<BleDevice>();
-		}
-		adapter=new ArrayAdapter<BleDevice>(this, android.R.layout.simple_list_item_1, deviceList);
-		bleList.setAdapter(adapter);
+		//		bleList=findViewById(R.id.main_BleList);
 		
 		ViewStub infoStub=findViewById(R.id.main_infoStub);
 		if (infoStub!=null) {
 			callbackText=infoStub.inflate().findViewById(R.id.tv_callback);
 		}
-		
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//			if (bluetoothManager==null) {
-//			   bluetoothManager= (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-//			}
-			
+
 			bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
 			if (bluetoothAdapter.isEnabled()) {
 				blePrepared=true;
 			}else {
 				blePrepared=false;
 			}
-			
+
 			if (blePrepared) {
 				setMTextColor("正在扫描蓝牙设备...", "#ff00ff00");
+				scanBtn.setText("正在扫描...");
+				
+				if (pointX==null) {
+					pointX=new ArrayList<Integer>();
+				}
+				if (pointY==null) {
+					pointY=new ArrayList<Integer>();
+				}
+				pointX.clear();
+				pointY.clear();
+				
+				if (deviceList==null) {
+					deviceList=new ArrayList<BleDevice>();
+				}
+				
+				if (bleList!=null) {
+					bleList.setVisibility(View.GONE);
+				}
+				
+				if (scanView==null) {			
+					ViewGroup.LayoutParams scanViewLP=new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+							ViewGroup.LayoutParams.MATCH_PARENT);
+					scanView=new ScanView(this);
+					scanView.setLayoutParams(scanViewLP);
+					container.addView(scanView);
+					
+					scanView.setOnPointUpdateListener(new OnPointUpdateListener() {
+						
+						@Override
+						public void OnUpdate(Canvas canvas, Paint paintPoint, float cx, float cy, float radius) {
+							// TODO Auto-generated method stub
+							for (int i = 0; i < pointX.size(); i++) {
+								canvas.drawCircle(cx+pointX.get(i), cy+pointY.get(i), 5, paintPoint);
+							}
+						}
+					});
+				}else {
+					if (scanView.getVisibility()==View.GONE) {
+						scanView.setVisibility(View.VISIBLE);
+					}
+				}	
+				scanView.start();
+				
 				if (scanThread==null) {
 					scanThread=new ScanThread(bluetoothAdapter, handler);
 				}
@@ -126,13 +200,13 @@ public class MainActivity extends Activity {
 			setMTextColor("当前系统不支持该功能", "#ffff0000");
 		}
 	}
-	
+
 	private void setMTextColor(String text, String color){
 		callbackText.setText(text);
 		callbackText.setTextColor(Color.WHITE);
 		callbackText.setBackgroundColor(Color.parseColor(color));
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
@@ -168,24 +242,24 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@SuppressLint("NewApi")
 	private class ScanThread extends Thread{
-		
+
 		private BluetoothLeScanner bluetoothLeScanner;
 		private Handler sHandler;
-		private static final int TIME_OUT=15;
-		
+		private static final int TIME_OUT=10;
+
 		public ScanThread(BluetoothAdapter bluetoothAdapter, Handler handler) {
 			// TODO Auto-generated constructor stub
 			this.sHandler=handler;
 			bluetoothLeScanner=bluetoothAdapter.getBluetoothLeScanner();
 		}
-		
+
 		private ScanCallback callback=new ScanCallback() {
-			
+
 			public void onScanResult(int callbackType, android.bluetooth.le.ScanResult result) {
-				
+
 				BluetoothDevice device=result.getDevice();
 				boolean isExits=false;
 				for(BleDevice info: deviceList){
@@ -193,23 +267,23 @@ public class MainActivity extends Activity {
 						isExits=true;
 					}
 				}
-		
+
 				if (!isExits) {
 					deviceList.add(new BleDevice(device.getAddress(), "", result.getRssi()));
 					handler.sendEmptyMessage(A_NEW_DEVICE_SCANED);
 				}
 			};
 		};
-		
+
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-//			super.run();
+			//			super.run();
 			deviceList.clear();
 			bluetoothLeScanner.startScan(callback);
-			
+
 			sHandler.postDelayed(new Runnable() {
-				
+
 				@Override
 				public void run() {
 					// TODO Auto-generated method stub
@@ -220,5 +294,5 @@ public class MainActivity extends Activity {
 		}
 	}
 
-		
+
 }
